6a38619bd07e25da14818e94ea5ffcaa1938108e
FlyingDonkey <jsosa@flyingdonkey.es>
2016-09-19 14:13:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ var PushNotification = {
 
     init: function(options) {
         options = options || {};
+        if (options.apn) options.apn.gateway = 'gateway.sandbox.push.apple.com';
         PushNotification.options = {apn: options.apn || null, gcm: options.gcm || null};
         PushNotification.clear();
     },
@@ -39,18 +40,52 @@ var PushNotification = {
         else if (type === DeviceType.ANDROID) PushNotification.tokens.gcm.push(token);
     },
 
-    push: function() {
+    push: function(callback) {
         var title = PushNotification.payload.title;
         var message = PushNotification.payload.message;
         var badge = PushNotification.payload.badge;
         var sound = PushNotification.payload.sound;
         var payload = PushNotification.payload.payload;
-        PushNotification.pushToAPN(PushNotification.tokens.apn, title, message, badge, sound, payload);
-        PushNotification.pushToGCM(PushNotification.tokens.gcm, title, message, badge, sound, payload);
+        var result = {
+            ios: null,
+            android: null
+        };
+        PushNotification.pushToAPN(PushNotification.tokens.apn, title, message, badge, sound, payload, function(res){
+            result.ios = res;
+        });
+        PushNotification.pushToGCM(PushNotification.tokens.gcm, title, message, badge, sound, payload, function(res){
+            result.android = res;
+        });
+
+        if(typeof callback == 'function'){
+            var i = 0;
+            var interval = setInterval(function () {
+                i++;
+                if(i == 50){
+                    clearInterval(interval);
+                }
+                if(result.android != null && result.ios != null){
+                    clearInterval(interval);
+                    callback(result);
+                }
+            }, 500);
+        }
+
     },
 
-    pushToAPN: function(tokens, title, message, badge, sound, payload) {
-        if (!PushNotification.options.apn || !tokens.length) return;
+    pushToAPN: function(tokens, title, message, badge, sound, payload, callback) {
+        if (!PushNotification.options.apn){
+            if(typeof callback == 'function') {
+                callback({message: 'No se a especificado APN'});
+            }
+            return;
+        }
+        if(!tokens.length){
+            if(typeof callback == 'function') {
+                callback({message: 'No hay tokens'});
+            }
+            return;
+        }
         var connection = new apn.Connection(PushNotification.options.apn);
         tokens.map(function(token) {
             var device = new apn.Device(token);
@@ -58,18 +93,44 @@ var PushNotification = {
             notification.title = title;
             notification.alert = message;
 			notification.badge = badge;
-			notification.sound = sound;
+			notification.sound = 'default';
 			notification.payload = {payload: payload};
 			connection.pushNotification(notification, device);
         });
+        if(typeof callback == 'function') {
+            callback({message: 'Done', data: 'IOS no tiene callback para mandar sus notificaciones'});
+        }
     },
 
-    pushToGCM: function(tokens, title, message, badge, sound, payload) {
-        if (!PushNotification.options.gcm || !tokens.length) return;
+    pushToGCM: function(tokens, title, message, badge, sound, payload, callback) {
+        if (!PushNotification.options.gcm){
+            if(typeof callback == 'function') {
+                callback({message: 'No se a especificado GCM'});
+            }
+            return;
+        }
+        if(!tokens.length){
+            if(typeof callback == 'function') {
+                callback({message: 'No hay tokens'});
+            }
+            return;
+        }
         var notification = new gcm.Message();
         notification.addData({title: title, message: message, payload: payload});
         var sender = new gcm.Sender(PushNotification.options.gcm.apiKey);
-        sender.send(notification, tokens);
+        sender.send(notification, tokens, function(err, response){
+            if(err){
+                if(typeof callback == 'function'){
+                    callback({message: 'Error', data: err});
+                }
+                //console.error('pushToGCM ERROR: ', err);
+            } else {
+                if(typeof callback == 'function') {
+                    callback({message: 'Done', data: response});
+                }
+                //console.log('pushToGCM DONE: ', response);
+            }
+        });
     },
 
     pushSingle: function(type, token, title, message, badge, sound, payload) {
